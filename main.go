0df8b1c4e84b19b8b5715953dc8bc9f49e485314
Hossein <hossein76rahimi@gmail.com>
2020-05-27 22:57:54 +0430

    Fix: json struct bug

--- main.go
+++ main.go
@@ -9,29 +9,32 @@ import (
 	"net/http"
 	"github.com/gorilla/mux"
 	"log"
+	// "strings"
 )
 var psqlInfo = fmt.Sprintf("host=%s port=%d user=%s "+
 	"password=%s dbname=%s sslmode=disable",
 	"localhost", 5432, "hossein", "abcd@1234", "go_db")
 type User struct {
-	age  int
-	email string
-	first_name string
-	last_name string
+	Age  int `"json":"age"`
+	Email string `"json":"email"`
+	First_name string `"json":"first_name"`
+	Last_name string `"json":"last_name"`
 }
 func mainPage(w http.ResponseWriter, r *http.Request){
+	fmt.Println(r.Body)
 	b, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
-
 	var user User
 	err = json.Unmarshal(b, &user)
+	fmt.Println(user)
 	if err != nil {
 		panic(err)
 	}
+
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
@@ -40,12 +43,13 @@ func mainPage(w http.ResponseWriter, r *http.Request){
 	sqlStatement := `
 	INSERT INTO users (age, email, first_name, last_name)
 	VALUES ($1, $2, $3, $4)`
-	_, err = db.Exec(sqlStatement, user.age, user.email, user.first_name, user.last_name)
+	_, err = db.Exec(sqlStatement, user.Age, user.Email, user.First_name, user.Last_name)
 	if err != nil {
 		panic(err)
 	}
+	data := fmt.Sprintf(`{"email": %s, "age": %d}`, user.Email, user.Age)
 
-	json.NewEncoder(w).Encode(`{"asdf":"asdf"}`)
+	json.NewEncoder(w).Encode(data)
 }
 
 func getPage(w http.ResponseWriter, r *http.Request){
